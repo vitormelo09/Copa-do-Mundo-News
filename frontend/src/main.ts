fcb3dac8a7d8d23f342288a2dd44eed7ea7ef4e5
Carlos150806 <carlos.eduardo15080622@gmail.com>
2026-09-17 13:47:03 -0300

    criação das pastas e ajuste do css

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,34 @@
 import './style.css'
 
-//HTMLDivElement: é informar ao TypeScript qual tipo de elemento queremos receber.
 const app = document.querySelector<HTMLDivElement>('#app')
 
-//ver se o elemento app foi encontrado
+//
 if (app) {
   app.innerHTML = `
-    <h1>Copa Feminina 2027</h1>
-    <p>Seu portal para acompanhar a Copa do Mundo Feminina.</p>
+    <header class="topbar">
+      <div class="brand">
+        <div class="brand-icon">⚽</div>
+
+        <div class="brand-name">
+          COPA DO MUNDO NEWS
+        </div>
+
+        <span class="year-badge">2027</span>
+      </div>
+
+      <nav class="menu">
+        <a href="#" class="active">Início</a>
+        <a href="#">Partidas</a>
+        <a href="#">Seleções</a>
+        <a href="#">Jogadoras</a>
+        <a href="#">Notícias</a>
+        <a href="#">Estádios</a>
+      </nav>
+
+      <div class="auth-actions">
+        <a href="#" class="login-link">Entrar</a>
+        <a href="#" class="create-account">Criar conta</a>
+      </div>
+    </header>
   `
 }
